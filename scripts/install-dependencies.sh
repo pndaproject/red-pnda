bf01bea92d433bfc63e536c7f7ff73bc26b381a7
Pavan Sudheendra <pasudhee@cisco.com>
2018-01-22 12:14:00 +0000

    Fix JDK download issue

--- scripts/install-dependencies.sh
+++ scripts/install-dependencies.sh
@@ -54,8 +54,18 @@ sudo apt-get install -y nodejs
 sudo apt-get install -y python-software-properties debconf-utils
 sudo add-apt-repository -y ppa:webupd8team/java
 sudo apt-get update
+
 echo "oracle-java8-installer shared/accepted-oracle-license-v1-1 select true" | sudo debconf-set-selections
+sudo apt-get install -y oracle-java8-installer || true
+
+# temporary workaround to fix the webupd jdk151 error -> https://ubuntuforums.org/showthread.php?t=2374686&page=4&p=13731177#post13731177
+sudo sed -i 's|JAVA_VERSION=8u151|JAVA_VERSION=8u162|' /var/lib/dpkg/info/oracle-java8-installer.*
+sudo sed -i 's|PARTNER_URL=http://download.oracle.com/otn-pub/java/jdk/8u151-b12/e758a0de34e24606bca991d704f6dcbf/|PARTNER_URL=http://download.oracle.com/otn-pub/java/jdk/8u162-b12/0da788060d494f5095bf8624735fa2f1/|' /var/lib/dpkg/info/oracle-java8-installer.*
+sudo sed -i 's|SHA256SUM_TGZ="c78200ce409367b296ec39be4427f020e2c585470c4eed01021feada576f027f"|SHA256SUM_TGZ="68ec82d47fd9c2b8eb84225b6db398a72008285fafc98631b1ff8d2229680257"|' /var/lib/dpkg/info/oracle-java8-installer.*
+sudo sed -i 's|J_DIR=jdk1.8.0_151|J_DIR=jdk1.8.0_162|' /var/lib/dpkg/info/oracle-java8-installer.*
+
 sudo apt-get install -y oracle-java8-installer
+sudo apt-get install -y oracle-java8-set-default
 
 # virtualbox guest additions
 # sudo mount /dev/cdrom /media/cdrom
